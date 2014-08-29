ee8c51b5fecf18f3c54633ab7efe626db2b600a2
gavinboy <yuanzeyao@163.com>
2014-08-29 16:35:36 +0800

    *):添加常用常量

--- src/com/yzy/im/server/IConstants.java
+++ src/com/yzy/im/server/IConstants.java
@@ -2,6 +2,7 @@ package com.yzy.im.server;
 
 public interface IConstants
 {
+  //请求参数
   public static final String METHOD="method";
   
   public static final String APIKEY="apikey";
@@ -23,6 +24,52 @@ public interface IConstants
   public static final String V="v";
   
   public static final String PUSH_TYPE="push_type";
+  
+  public static final String CHANNEL_ID="channel_id";
+  
+  public static final String TAG="tag";
+  
+  public static final String MESSAGE_TYPE="message_type";
+  
+  public static final String MESSAGES="messages";
+  
+  public static final String MSG_KEYS="msg_keys";
+  
+  public static final String MESSAGE_EXPIRES="message_expires";
+  
+  public static final String DEPLOY_STATUS="deploy_status";
+  
+  public static final String MSG_IDS="msg_ids";
+  
+  
+  //方法名
+  public static final String QUERY_BINDLIST="query_bindlist";
+  
+  public static final String PUSH_MSG="push_msg";
+  
+  public static final String VERIFY_BIND="verify_bind";
+  
+  public static final String FETCH_MSG="fetch_msg";
+  
+  public static final String FETCH_MSGCOUNT="fetch_msgcount";
+  
+  public static final String DELETE_MSG="delete_msg";
+  
+  public static final String SET_TAG="set_tag";
+  
+  public static final String FETCH_TAG="fetch_tag";
+  
+  public static final String DELETE_TAG="delete_tag";
+  
+  public static final String QUERY_USER_TAGS="query_user_tags";
+  
+  public static final String QUERY_DEVICE_TYPE="query_device_type";
+  
+  public static final String API_KEY="v39ZGSe6lOZlPLGAQyVeCmCd";
+  
+  public static final String  SECRET_KEY="oAAlQzOEaNDvDskvelNduk8PbAzklTXu";
+  
+  
   
   
   
